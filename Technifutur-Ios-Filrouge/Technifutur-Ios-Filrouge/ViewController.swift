b819b81fa8c79f5760ac68f18667629795fa655c
Cedricdebroux <cedric@debroux.ovh>
2022-07-05 13:51:41 +0200

    create page login

--- Technifutur-Ios-Filrouge/Technifutur-Ios-Filrouge/ViewController.swift
+++ Technifutur-Ios-Filrouge/Technifutur-Ios-Filrouge/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+ 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            view.endEditing(true)
+        }
 
 }
 
+extension ViewController: UITextFieldDelegate{
+
+    func textFieldShouldReturn(_ textField: UITextField)->Bool{
+        view.endEditing(true)
+        
+
+        return true
+    }
+}
